a4b15d690d75beaf5afcc673c9322bf1992a7fbc
Omega050 <mauro-jr50@hotmail.com>
2023-08-15 22:21:57 -0300

    aa

--- Constant.js
+++ Constant.js
@@ -1,6 +1,6 @@
 let x=10
 const y=10
 x=20
-y=20 //A tentativa de alteração do valor da constante gerará um erro na tentativa de executar o programa
+y=20 //A tentativa de alteração do valor da constante gerará um erro ao executar o programa
 console.log(x)
 console.log(y)
