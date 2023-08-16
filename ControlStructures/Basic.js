d9098de85080b137376b54a6f32a632f39bb250b
Omega050 <mauro-jr50@hotmail.com>
2023-08-16 11:29:49 -0300

    Almost done

--- ControlStructures/Basic.js
+++ ControlStructures/Basic.js
@@ -26,8 +26,6 @@ while (condition) {
     }
     // Código aqui será executado
   }
-
-
   
   for (let i = 0; i < 10; i++) {
     if (i === 5) {
@@ -35,3 +33,34 @@ while (condition) {
     }
     // Código aqui será executado, exceto quando i for 5
   }
+
+//Switch
+
+  const dayOfWeek = 3; // 1 = Domingo, 2 = Segunda, 3 = Terça, ...
+
+switch (dayOfWeek) {
+  case 1:
+    console.log("Hoje é domingo");
+    break;
+  case 2:
+    console.log("Hoje é segunda-feira");
+    break;
+  case 3:
+    console.log("Hoje é terça-feira");
+    break;
+  case 4:
+    console.log("Hoje é quarta-feira");
+    break;
+  case 5:
+    console.log("Hoje é quinta-feira");
+    break;
+  case 6:
+    console.log("Hoje é sexta-feira");
+    break;
+  case 7:
+    console.log("Hoje é sábado");
+    break;
+  default:
+    console.log("Dia inválido");
+    break;
+}
